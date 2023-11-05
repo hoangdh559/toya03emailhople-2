365e45166346222c9da390260a78b5b781994218
hoangdh559 <149799805+hoangdh559@users.noreply.github.com>
2023-11-05 14:36:48 +0000

    hoàn tất

--- s00_bailam.py
+++ s00_bailam.py
@@ -30,10 +30,26 @@ get_name_in_email([None])                                 | ['ERROR invaid email
 get_name_in_email([None, 'abb#ccc'])                      | ['ERROR invaid email', 'ERROR invaid email']
 """
 #endregion debai
+import re
 
 #region bailam
 def get_name_in_email(email_list):
-  return 'todo'
+  regex = r"([\w-]+\.)*[\w-]+@[\w-]+\.[\w-]+"
+  arr = []
+  if not email_list:
+      return arr
+  else:
+      for email in email_list:
+        if email is None:
+            arr.append("ERROR invaid email")
+        elif not re.match(regex, email):
+              arr.append("ERROR invaid email")
+
+          # Tách tên tài khoản và miền email
+        else:
+              name, domain = email.split("@")
+              arr.append(name)
+      return arr
   
 
 if __name__=='__main__':
